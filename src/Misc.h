aaef325149c8fb27f6a26aee14228dba5b771675
igor@shg.ru <igor@shg.ru@10c4dfc4-00c9-b076-3360-45a67c2f8363>
2012-01-18 17:30:08 +0000

    pomerol: new values for ObjectStatus (Loaded, Stored)
    git-svn-id: https://pomerol.googlecode.com/svn/trunk@976 10c4dfc4-00c9-b076-3360-45a67c2f8363

--- src/Misc.h
+++ src/Misc.h
@@ -83,7 +83,7 @@ typedef unsigned short ParticleIndex;
 enum spin {down, up};
 
 /** This represent the progress of calculation of any complex object in the code */
-enum ObjectStatus {Constructed, Prepared, Computed};
+enum ObjectStatus {Constructed, Prepared, Loaded, Computed, Stored};
 
 /** A short name for imaginary unit. */
 static const ComplexType I = ComplexType(0.0,1.0);    // 'static' to prevent linking problems
