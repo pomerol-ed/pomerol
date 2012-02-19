ee23aed336524bcd6ba5206736f24c946d2c0b7b
Andrey.E.Antipov@gmail.com <Andrey.E.Antipov@gmail.com@10c4dfc4-00c9-b076-3360-45a67c2f8363>
2012-02-19 01:48:29 +0000

    Added tests as a target, make check implemented
    git-svn-id: https://pomerol.googlecode.com/svn/trunk@1020 10c4dfc4-00c9-b076-3360-45a67c2f8363

--- tests/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env sh
-
-# TODO: Remove me!!!
-
-CFLAGS="-I../src -I/usr/include/eigen3 -I../jsoncpp/include -L../src -L../jsoncpp/src -lpomerol -ljsoncpp -lhdf5 -lhdf5_cpp -fopenmp"
-g++ green.cpp -o green ${CFLAGS}
-g++ gamma4.cpp -o gamma4 ${CFLAGS}
