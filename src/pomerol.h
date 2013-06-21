57081cf25469825ac958f676126b0c8d1356ac25
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2013-06-21 14:55:55 +0200

    Created global header pomerol.h

---
+++ src/pomerol.h
@@ -0,0 +1,51 @@
+//
+// This file is a part of pomerol - a scientific ED code for obtaining 
+// properties of a Hubbard model on a finite-size lattice 
+//
+// Copyright (C) 2010-2013 Andrey Antipov <Andrey.E.Antipov@gmail.com>
+// Copyright (C) 2010-2013 Igor Krivenko <Igor.S.Krivenko@gmail.com>
+//
+// pomerol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// pomerol is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pomerol.  If not, see <http://www.gnu.org/licenses/>.
+
+/** \file src/pomerol.h
+** \brief include file for all pomerol library
+**
+** \author Andrey Antipov <Andrey.E.Antipov@gmail.com> 
+*/
+
+
+
+#ifndef __INCLUDE_POMEROL_H_19f82k
+#define __INCLUDE_POMEROL_H_19f82k
+
+#include "Misc.h"
+#include "Logger.h"
+#include "Lattice.h"
+#include "LatticePresets.h"
+#include "Index.h"
+#include "IndexClassification.h"
+#include "Operator.h"
+#include "OperatorPresets.h"
+#include "IndexHamiltonian.h"
+#include "Symmetrizer.h"
+#include "StatesClassification.h"
+#include "Hamiltonian.h"
+#include "FieldOperator.h"
+#include "FieldOperatorContainer.h"
+#include "DensityMatrix.h"
+#include "GFContainer.h"
+#include "TwoParticleGFContainer.h"
+
+
+#endif
