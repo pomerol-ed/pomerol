a948c088493bbc6cfa6bd21451faafd29b8781f6
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2013-12-21 02:07:14 -0500

    Updated FindPomerol

--- cmake/FindPomerol.cmake
+++ cmake/FindPomerol.cmake
@@ -1,4 +1,4 @@
-#  Try to find GFTools. Once done this will define
+#  Try to find Pomerol. Once done this will define
 #  Pomerol_FOUND - System has GFTools
 #  Pomerol_INCLUDE_DIRS - The GFTools include directories
 #  Pomerol_DEFINITIONS - Compiler switches required for using GFTools
@@ -8,12 +8,13 @@ pkg_check_modules(PC_Pomerol QUIET pomerol)
 set(Pomerol_DEFINITIONS ${PC_Pomerol_CFLAGS_OTHER})
 
 #include
-find_path(Pomerol_INCLUDE_DIR GridObject.hpp
-          HINTS ${PC_Pomerol_INCLUDEDIR} ${PC_Pomerol_INCLUDE_DIRS} ${EIGEN3_INCLUDE_DIR} 
+find_path(Pomerol_INCLUDE_DIR TwoParticleGF.h
+          HINTS ${PC_Pomerol_INCLUDEDIR} ${PC_Pomerol_INCLUDE_DIRS} ${EIGEN3_INCLUDE_DIR} "${Pomerol_ROOT}/include/pomerol" 
          )
 #lib
+message (STATUS ${Pomerol_ROOT}/include/pomerol)
 find_library(Pomerol_LIBRARY NAMES pomerol libpomerol
-             HINTS ${PC_Pomerol_LIBDIR} ${PC_Pomerol_LIBRARY_DIRS} )
+             HINTS ${PC_Pomerol_LIBDIR} ${PC_Pomerol_LIBRARY_DIRS} ${Pomerol_ROOT}/lib)
 
 set(Pomerol_INCLUDE_DIRS ${Pomerol_INCLUDE_DIR} )
 set(Pomerol_LIBRARIES ${Pomerol_LIBRARY})
@@ -21,6 +22,6 @@ set(Pomerol_LIBRARIES ${Pomerol_LIBRARY})
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set Pomerol_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(GFTools "No GFTools found" Pomerol_LIBRARY Pomerol_INCLUDE_DIR)
+find_package_handle_standard_args(GFTools "No Pomerol found" Pomerol_LIBRARY Pomerol_INCLUDE_DIR)
 
-mark_as_advanced(Pomerol_INCLUDE_DIR Pomerol_DEFINITIONS)
+mark_as_advanced(Pomerol_INCLUDE_DIR Pomerol_INCLUDE_DIRS Pomerol_LIBRARY Pomerol_LIBRARIES Pomerol_DEFINITIONS)
