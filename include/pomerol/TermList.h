c9f30c933a57b2f967500b4b28d666f7eb3e3a59
Igor Krivenko <igor.s.krivenko@gmail.com>
2017-06-08 17:58:42 -0400

    Document TermList class

--- include/pomerol/TermList.h
+++ include/pomerol/TermList.h
@@ -35,9 +35,19 @@
 
 namespace Pomerol {
 
+/** Container storing a list of terms (instances of TermType).
+ *
+ * The terms must allow sorting using a comparison function TermType::Compare.
+ * Like terms (equivalent w.r.t. TermType::Compare) are automatically collected
+ * and reduced to one term using operator+=().
+ * A term T is considered negligible and is automatically removed from the
+ * container if TermType::IsNegligible(T, current_number_of_terms + 1) evaluates to true.
+ */
 template<typename TermType> class TermList {
 
+    /** Comparison function */
     typedef typename TermType::Compare Compare;
+    /** Predicate for determining negligible terms */
     typedef typename TermType::IsNegligible IsNegligible;
 
     std::set<TermType, Compare> data;
@@ -45,9 +55,16 @@ template<typename TermType> class TermList {
 
 public:
 
+    /** Constructor.
+     * \param[in] compare Compare predicate for the underlying std::set object
+     * \param[in] is_negligible Predicate that determines whether a term can be neglected
+     */
     TermList(Compare const& compare, IsNegligible const& is_negligible) :
         data(compare), is_negligible(is_negligible) {}
 
+    /** Add a new term to the container
+     * \param[in] term Term to be added
+     */
     void add_term(TermType const& term) {
         typename std::set<TermType, Compare>::iterator it = data.find(term);
         if(it == data.end()) { // new term
@@ -61,8 +78,10 @@ public:
         }
     }
 
+    /** Number of terms in the container */
     std::size_t size() const { return data.size(); }
 
+    /** Remove all terms from the container */
     void clear() { data.clear(); }
 
     // Some pre-C++11 ugliness ...
@@ -77,17 +96,22 @@ public:
         return res;                                                         \
     }
 
+    /** Pass arguments to operator() of each term in the container
+     *  and return a sum of their return values.
+     */
     MAKE_CALL_OPERATOR(1)
     MAKE_CALL_OPERATOR(2)
     MAKE_CALL_OPERATOR(3)
     MAKE_CALL_OPERATOR(4)
 #undef MAKE_CALL_OPERATOR
 
+    /** Boost.Serialization interface */
     friend class boost::serialization::access;
     template<class Archive> void serialize(Archive & ar, const unsigned int version) {
         ar & data; ar & is_negligible;
     }
 
+    /** Check that all terms in the container are properly ordered and are not negligible */
     bool check_terms() {
         if(size() == 0) return true;
         typename std::set<TermType>::const_iterator prev_it = data.begin();
