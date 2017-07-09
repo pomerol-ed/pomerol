c3b7fe1a466414f12b0c2127306ba43e034a5a40
Andrey E. Antipov <Andrey.E.Antipov@gmail.com>
2017-07-09 14:53:26 -0600

    cleanup include

--- include/pomerol/GFContainer.h
+++ include/pomerol/GFContainer.h
@@ -1,25 +1,5 @@
-//
-// This file is a part of pomerol - a scientific ED code for obtaining 
-// properties of a Hubbard model on a finite-size lattice 
-//
-// Copyright (C) 2010-2012 Andrey Antipov <Andrey.E.Antipov@gmail.com>
-// Copyright (C) 2010-2012 Igor Krivenko <Igor.S.Krivenko@gmail.com>
-//
-// pomerol is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// pomerol is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with pomerol.  If not, see <http://www.gnu.org/licenses/>.
-
-
 /** \file src/GFContainer.h
+** \brief Storage of GF for multiple indices (obsolete, remove)
 **
 ** \author Andrey Antipov (Andrey.E.Antipov@gmail.com)
 ** \author Igor Krivenko (Igor.S.Krivenko@gmail.com)
