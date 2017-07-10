1a8277f647031b91904cdb733bcda17b77672183
Andrey E. Antipov <Andrey.E.Antipov@gmail.com>
2017-07-09 18:19:34 -0700

    src : strip first comments and license

--- src/pomerol/Operator.cpp
+++ src/pomerol/Operator.cpp
@@ -1,30 +1,3 @@
-//
-// This file is a part of pomerol - a scientific ED code for obtaining
-// properties of a Hubbard model on a finite-size lattice
-//
-// Copyright (C) 2010-2011 Andrey Antipov <Andrey.E.Antipov@gmail.com>
-// Copyright (C) 2010-2011 Igor Krivenko <Igor.S.Krivenko@gmail.com>
-//
-// pomerol is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// pomerol is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with pomerol.  If not, see <http://www.gnu.org/licenses/>.
-
-
-/** \file Operator.cpp
-**  \brief Implementation of the Operator, Operator::Term classes
-**
-**  \author    Andrey Antipov (Andrey.E.Antipov@gmail.com)
-*/
-
 #include "pomerol/Operator.h"
 #include <algorithm>
 #include <iterator>
