6dad6678895e1a81036c60cdaa3342f7ab0cb174
Nils Wentzell <nwentzell@flatironinstitute.org>
2018-10-17 11:38:20 -0400

    [cmake] Consider relevant environment variables in FindEigen3.cmake

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -63,6 +63,9 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
+      $ENV{EIGEN_ROOT}/include
+      ENV CPATH
+      ENV CPLUS_INCLUDE_PATH
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
