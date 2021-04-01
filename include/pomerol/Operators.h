066817e7cc26ef83cabe63a7cf365f6f16b18237
Igor Krivenko <igor.s.krivenko@gmail.com>
2021-04-01 14:22:46 +0200

    libcommute: New header Operators.h
    This header features a new namespace, Pomerol::Operators imports a few
    libcommute classes and functions.

---
+++ include/pomerol/Operators.h
@@ -0,0 +1,23 @@
+/** \file Operators.h
+**  \brief Expressions with quantum-mechanical operators and functions to
+*   construct them.
+**
+**  \author    Igor Krivenko (Igor.S.Krivenko@gmail.com)
+*/
+
+#include <libcommute/expression/expression.hpp>
+#include <libcommute/expression/factories.hpp>
+#include <libcommute/expression/hc.hpp>
+
+namespace Pomerol {
+namespace Operators {
+
+using libcommute::expression;
+using libcommute::hc;
+
+using libcommute::static_indices::c;
+using libcommute::static_indices::c_dag;
+using libcommute::static_indices::n;
+
+} // end of namespace Pomerol::Operators
+} // end of namespace Pomerol
