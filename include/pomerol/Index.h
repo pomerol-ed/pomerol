2694807ba6b7cc50d341c85b177887c238222216
Igor Krivenko <igor.s.krivenko@gmail.com>
2021-05-30 18:09:31 +0300

    Move all remaining code from Index.cpp to the header

--- include/pomerol/Index.h
+++ include/pomerol/Index.h
@@ -11,6 +11,7 @@
 #include"Misc.h"
 
 #include <ostream>
+#include <tuple>
 
 namespace Pomerol {
 
@@ -24,17 +25,31 @@ struct IndexCombination2
      * \param[in] Index1 - Index of a 1st operator (C)
      * \param[in] Index2 - Index of a 2nd operator (C^+)
     */
-    IndexCombination2(ParticleIndex Index1, ParticleIndex Index2);
+    IndexCombination2(ParticleIndex Index1, ParticleIndex Index2) :
+        Index1(Index1), Index2(Index2) {}
 
     /** Operator < - comparison method for IndexCombination */
-    bool operator< (const IndexCombination2& rhs) const ;
+    bool operator< (const IndexCombination2& rhs) const
+    {
+        return std::make_tuple(Index1, Index2) <
+               std::make_tuple(rhs.Index1, rhs.Index2);
+    }
     /** Operator == */
-    bool operator==(const IndexCombination2& rhs) const ;
+    bool operator==(const IndexCombination2& rhs) const
+    {
+        return Index1 == rhs.Index1 && Index2 == rhs.Index2;
+    }
     /** Operator != */
-    bool operator!=(const IndexCombination2& rhs) const ;
+    bool operator!=(const IndexCombination2& rhs) const
+    {
+        return !operator==(rhs);
+    }
 
     /** Output to external stream */
-    friend std::ostream& operator<<(std::ostream& output, const IndexCombination2& out);
+    friend std::ostream& operator<<(std::ostream& os, const IndexCombination2& ic)
+    {
+        return os << "(" << ic.Index1 << ic.Index2 << ")";
+    }
 };
 
 /** A structure to handle a combination of 4 Particle Indices. */
@@ -50,17 +65,34 @@ struct IndexCombination4
      * \param[in] Index4 - Index of a 4nd operator (C^+)
     */
     IndexCombination4(ParticleIndex Index1, ParticleIndex Index2,
-                     ParticleIndex Index3, ParticleIndex Index4);
+                     ParticleIndex Index3, ParticleIndex Index4) :
+        Index1(Index1), Index2(Index2), Index3(Index3), Index4(Index4) {}
 
     /** Operator < - comparison method for IndexCombination */
-    bool operator< (const IndexCombination4& rhs) const ;
+    bool operator< (const IndexCombination4& rhs) const
+    {
+        return std::make_tuple(Index1, Index2, Index3, Index4) <
+               std::make_tuple(rhs.Index1, rhs.Index2, rhs.Index3, rhs.Index4);
+    }
     /** Operator == */
-    bool operator==(const IndexCombination4& rhs) const ;
+    bool operator==(const IndexCombination4& rhs) const
+    {
+        return Index1 == rhs.Index1 &&
+               Index2 == rhs.Index2 &&
+               Index3 == rhs.Index3 &&
+               Index4 == rhs.Index4;
+    }
     /** Operator != */
-    bool operator!=(const IndexCombination4& rhs) const ;
+    bool operator!=(const IndexCombination4& rhs) const
+    {
+        return !operator==(rhs);
+    }
 
     /** Output to external stream */
-    friend std::ostream& operator<<(std::ostream& output, const IndexCombination4& out);
+    friend std::ostream& operator<<(std::ostream& os, const IndexCombination4& ic)
+    {
+        return os << "(" << ic.Index1 << ic.Index2 << ic.Index3 << ic.Index4 << ")";
+    }
 };
 
 } // namespace Pomerol
