7b8a80527f65e9cae00c68edd7cd78a3f56d5355
Igor Krivenko <igor.s.krivenko@gmail.com>
2021-07-05 14:08:04 +0200

    [test] Port `AndersonTest` to Catch2 and make it actually check GF values

--- test/AndersonTest.cpp
+++ test/AndersonTest.cpp
@@ -1,47 +1,62 @@
 #include "Misc.hpp"
 #include "LatticePresets.hpp"
-#include "Operators.hpp"
 #include "IndexClassification.hpp"
 #include "HilbertSpace.hpp"
 #include "StatesClassification.hpp"
-#include "HamiltonianPart.hpp"
 #include "Hamiltonian.hpp"
 #include "DensityMatrix.hpp"
 #include "FieldOperatorContainer.hpp"
-#include "GFContainer.hpp"
+#include "GreensFunction.hpp"
 
-#include "./Utility.hpp"
-
-#include <vector>
+#undef INFO // Catch2 has its own INFO() macro
+#include "catch2/catch.hpp"
 
 using namespace Pomerol;
 
-RealType beta = 20;
-RealType U = 3.7;
-RealType mu = U/2*0;
-RealType h = 0.0;
-RealType V = 1.0;
-RealType epsilon = 2.3;
+TEST_CASE("Anderson model with 2 bath sites", "[Anderson]") {
+    RealType U = 3.7;
+    RealType mu = 0.6 * U;
+    RealType h = 0.1;
+    RealType V = 1.0;
+    RealType epsilon = 2.3;
+    RealType beta = 20;
 
-int main(int argc, char* argv[])
-{
-    MPI_Init(&argc, &argv);
+    // Reference Green's functions
+    ComplexVectorType G_ref_up(10);
+    G_ref_up << -0.7545439 -0.14723373*I,
+                -0.59517353-0.34478922*I,
+                -0.42646689-0.4031622*I,
+                -0.30758605-0.39519013*I,
+                -0.23068661-0.36811109*I,
+                -0.18013326-0.33885065*I,
+                -0.14541077-0.31208195*I,
+                -0.12043127-0.28860297*I,
+                -0.10171394-0.26815136*I,
+                -0.08721336-0.25025992*I;
+    ComplexVectorType G_ref_down(10);
+    G_ref_down << 0.49196891-0.07241433*I,
+                  0.44396903-0.18681652*I,
+                  0.37248532-0.24764566*I,
+                  0.30425235-0.26969548*I,
+                  0.24921656-0.27135953*I,
+                  0.20705484-0.26399883*I,
+                  0.1748209 -0.25319165*I,
+                  0.14976473-0.2414213*I,
+                  0.12986709-0.22973732*I,
+                  0.11373954-0.21855949*I;
 
     using namespace LatticePresets;
 
     auto HExpr = CoulombS("C", U, -mu);
-    HExpr += Magnetization("C", 2*h);
+    HExpr += Magnetization("C", h);
     HExpr += Level("0", -epsilon);
     HExpr += Level("1", epsilon);
     HExpr += Hopping("C", "0", V);
     HExpr += Hopping("C", "1", V);
+    INFO("Hamiltonian\n" << HExpr);
 
     auto IndexInfo = MakeIndexClassification(HExpr);
-    print_section("Indices");
-    std::cout << IndexInfo << std::endl;
-
-    INFO("Hamiltonian");
-    INFO(HExpr);
+    INFO("Indices\n" << IndexInfo);
 
     auto HS = MakeHilbertSpace(IndexInfo, HExpr);
     HS.compute();
@@ -50,13 +65,9 @@ int main(int argc, char* argv[])
 
     Hamiltonian H(S);
     H.prepare(HExpr, HS, MPI_COMM_WORLD);
-    for (BlockNumber i=0; i < S.getNumberOfBlocks(); i++) {
-        std::vector<QuantumState> st = S.getFockStates(i);
-        for (int i=0; i<st.size(); ++i) INFO(st[i]);
-        INFO("");
-    };
-
     H.compute(MPI_COMM_WORLD);
+    INFO("Energy levels " << H.getEigenValues());
+    INFO("The value of ground energy is " << H.getGroundEnergy());
 
     DensityMatrix rho(S, H, beta);
     rho.prepare();
@@ -66,34 +77,32 @@ int main(int argc, char* argv[])
     Operators.prepareAll(HS);
     Operators.computeAll();
 
-    ParticleIndex down_index = IndexInfo.getIndex("C", 0, down);
-    ParticleIndex up_index = IndexInfo.getIndex("C", 0, up);
-
-    DEBUG(down_index);
-    DEBUG(up_index);
-
-    std::cout << IndexInfo << std::endl;
-    for (ParticleIndex i=0; i<IndexInfo.getIndexSize(); i++) {
-        INFO("C^+_"<<i);
-    }
+    ParticleIndex C_down_index = IndexInfo.getIndex("C", 0, down);
+    ParticleIndex C_up_index = IndexInfo.getIndex("C", 0, up);
 
     GreensFunction GF_down(S, H,
-        Operators.getAnnihilationOperator(down_index),
-        Operators.getCreationOperator(down_index),
+        Operators.getAnnihilationOperator(C_down_index),
+        Operators.getCreationOperator(C_down_index),
     rho);
 
     GreensFunction GF_up(S, H,
-        Operators.getAnnihilationOperator(up_index),
-        Operators.getCreationOperator(up_index),
+        Operators.getAnnihilationOperator(C_up_index),
+        Operators.getCreationOperator(C_up_index),
     rho);
 
-    GF_down.prepare(); DEBUG(""); GF_up.prepare();
-    GF_down.compute(); DEBUG(""); GF_up.compute();
+    GF_down.prepare(); GF_up.prepare();
+    GF_down.compute(); GF_up.compute();
 
-    for (size_t i=0; i<10; i++) {
-        INFO(GF_down(i) << " " << GF_up(i));
+    for(int n = 0; n < G_ref_up.size(); ++n) {
+        auto result = GF_up(n);
+        auto ref = G_ref_up[n];
+        REQUIRE(result.real() == Approx(ref.real()).margin(1e-12));
+        REQUIRE(result.imag() == Approx(ref.imag()).margin(1e-12));
     }
-
-    MPI_Finalize();
-    return EXIT_SUCCESS;
+    for(int n = 0; n < G_ref_down.size(); ++n) {
+        auto result = GF_down(n);
+        auto ref = G_ref_down[n];
+        REQUIRE(result.real() == Approx(ref.real()).margin(1e-12));
+        REQUIRE(result.imag() == Approx(ref.imag()).margin(1e-12));
+    }
 }
