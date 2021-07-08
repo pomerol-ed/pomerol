ca9ccb93c6143f849eb5857a9662c1d07f6d597a
Igor Krivenko <igor.s.krivenko@gmail.com>
2021-07-08 18:19:13 +0200

    [test] Merge `TwoParticleGFContainerTest` into `Vertex4Test`

--- test/Utility.hpp
+++ test/Utility.hpp
@@ -6,9 +6,6 @@
 #include <iostream>
 #include <string>
 
-// Generalized 'square' function.
-template<typename T> inline T sqr(T x) { return x*x; }
-
 void print_section(const std::string& str)
 {
     if(!pMPI::rank(MPI_COMM_WORLD)) {
