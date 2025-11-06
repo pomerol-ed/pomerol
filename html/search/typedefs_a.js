79ba30e59baa6a688cef4df59399b911a9272966
krivenko <iskrivenko@proton.me>
2025-11-06 11:36:17 +0000

    Deploying to gh-pages from @ pomerol-ed/pomerol@af9686a7c9f76e048fe854295123b0475add2132 🚀

--- html/search/typedefs_a.js
+++ html/search/typedefs_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['spacepartitiontype_752',['SpacePartitionType',['../class_pomerol_1_1_hilbert_space.html#a3687b4e6e5af8ca54273d6b548e91d7b',1,'Pomerol::HilbertSpace']]]
+  ['spacepartitiontype_752',['SpacePartitionType',['../class_pomerol_1_1_hilbert_space.html#a0c6ac784895e741d12c26ed2c0a0fdf3',1,'Pomerol::HilbertSpace']]]
 ];
